4e2a9c7fc50d9cb49aee62b61916435883871857
David Jake Morfe <jakemorfe@gmail.com>
2021-08-28 15:48:51 -0400

    delete for git update to work

--- update_bot.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/sh
-pwd
-git fetch origin
-git pull origin main
-sleep 2
-systemctl restart botd
--- update_db.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/sh
-pwd
-cd /home/jake/MSA-Bot
-rm -f loot.txt
-/usr/bin/python3 verify.py && /usr/bin/python3 put.py
